43f69c7b00a3384fc00d7b3da4f889d5bb0098ba
AbhaySharma1969 <abhaysharma05@gmail.com>
2026-06-29 21:32:37 -0700

    Update Firebase config and export statement

--- PDXAIGarageV1_4/assets/js/firebase-config.js
+++ PDXAIGarageV1_4/assets/js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase config for PDX AI Garage LatestBuild.
 // Replace these values with your Firebase web app config before real login works.
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCd3nj3VQ97hrVgKWSYpFlo_Q8BLlMON0U",
+export const firebaseConfig = {
+  apiKey: "AIzaSyCd3nj3VQ97hrVgKWSYpFlo_Q8BLL1MON0U",
   authDomain: "pdxaigarage.firebaseapp.com",
   projectId: "pdxaigarage",
   storageBucket: "pdxaigarage.firebasestorage.app",
   messagingSenderId: "1021792081744",
-  appId: "1:1021792081744:web:83689db4efeefab94e3e55",
+  appId: "1:1021792081744:web:83689db4efeeefab94e3e55",
   measurementId: "G-W40NQMZJHG"
 };
